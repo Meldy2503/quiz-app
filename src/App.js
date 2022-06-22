4ae75adbe02aa7562020228fe095eb0d977d7467
meldy2503 <emelder.charles25@gmail.com>
2022-06-22 10:46:59 +0100

    style: modify project design and color

--- src/App.js
+++ src/App.js
@@ -1,105 +1,68 @@
-import React from 'react';
-import "./assets/style.css";
-import index from './quizService/index';
-import Question from './components/Questions';
-import Result from './components/Result';
+import React from "react";
+import "./components/style.css";
+import index from "./components/data";
+import Question from "./components/Questions";
+import Result from "./components/Result";
 
 class App extends React.Component {
-    state = {                           
-      questionBank: [],
-      score: 0, 
-      responses: 0
-    };
+  state = {
+    questionBank: [],
+    score: 0,
+    responses: 0,
+  };
 
-  //  function  App(props) {
-  //    const [state, setState] = React.useState({
-  //     questionBank: [],
-  //     score: 0, 
-  //     responses: 0
-  //    });
-    
-
-    getQuestions = () => {
-      index().then (question => {
-        this.setState ({
-          questionBank: question
-        });
-      });
-    };
-    
-    //  getQuestions = () => {
-    //   setState ({
-    //       questionBank: props.question
-    //     });
-    
-
-     computeAnswer = (answer, correctAnswer) => {
-      if (answer === correctAnswer) {
-       this.setState({
-         score: this.state.score + 1
-         });
-      }
+  getQuestions = () => {
+    index().then((question) => {
       this.setState({
-        responses: this.state.responses < 5 ? this.state.responses + 1 : 5
+        questionBank: question,
       });
-    };
-     playAgain = () => {
-      this.getQuestions();
+    });
+  };
+
+  computeAnswer = (answer, correctAnswer) => {
+    if (answer === correctAnswer) {
       this.setState({
-        score: 0,
-        responses: 0,
-      })
+        score: this.state.score + 1,
+      });
     }
-  
-       componentDidMount() {
-       this.getQuestions();
-        }
-    // React.useEffect(() => {
-    //   getQuestions();
-    // }, [])
+    this.setState({
+      responses: this.state.responses < 5 ? this.state.responses + 1 : 5,
+    });
+  };
+  playAgain = () => {
+    this.getQuestions();
+    this.setState({
+      score: 0,
+      responses: 0,
+    });
+  };
 
-     render () {
-      return (
-        <div className='container'>
-           <div className='title'>QuizBee</div>
-           {this.state.questionBank.length > 0 && 
-           this.state.responses < 5 &&
-           this.state.questionBank.map(
-            ({question, answers, correct, questionId}) => (
-        <Question 
-           key={questionId} 
-          question={question} 
-          options={answers}
-          selected={answer => this.computeAnswer(answer, correct)} 
-           /> )
-          )} 
-          {this.state.responses === 5 ? 
-          (<Result score={this.state.score} 
-            playAgain={this.playAgain} />) : null}
-        </div>
+  componentDidMount() {
+    this.getQuestions();
+  }
 
-      )
-    }
+  render() {
+    return (
+      <div className="container">
+        <div className="title">QuizBee</div>
+        {this.state.questionBank.length > 0 &&
+          this.state.responses < 5 &&
+          this.state.questionBank.map(
+            ({ question, answers, correct, questionId }) => (
+              <Question
+                key={questionId}
+                question={question}
+                options={answers}
+                selected={(answer) => this.computeAnswer(answer, correct)}
+              />
+            )
+          )}
+        {this.state.responses === 5 ? (
+          <Result score={this.state.score} playAgain={this.playAgain} />
+        ) : null}
+      </div>
+    );
+  }
 }
 
-// function App() {
-//   const [questionBank, setQuestionBank ] = React.useState({
-//          questions: ""
-//   })
-
-//   function quizQuestions() {
-//     const data = questionBank.qBank
-//     const getQuestions = data.question
-//     setQuestionBank({
-//       questions: getQuestions })
-//   }
-
-
-//    return (
-//      <div className='container'>
-//         <div className='title'>QuizBee</div>
-//           <h4>{questionBank.questions}</h4> 
-//      </div>
-//    )
-// }
 export default App;
